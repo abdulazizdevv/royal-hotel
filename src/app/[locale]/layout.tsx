4a17103d469a3d2a6acac4159247e81e5e541233
abdulazizdevv <abdulazizkenjaoxunov@gmail.com>
2024-10-26 21:59:15 +0500

    add: yandex metrika

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -103,6 +103,48 @@ export default async function LocaleLayout({
             <Footer />
           </Providers>
         </NextIntlClientProvider>
+
+        <script
+          type='text/javascript'
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){
+                (m[i].a=m[i].a||[]).push(arguments)
+              };
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) {
+                  return;
+                }
+              }
+              k=e.createElement(t),
+              a=e.getElementsByTagName(t)[0],
+              k.async=1,
+              k.src=r,
+              a.parentNode.insertBefore(k,a)
+            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(98737773, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true,
+              ecommerce:"dataLayer"
+            });
+          `,
+          }}
+        />
+        <noscript>
+          <div>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src='https://mc.yandex.ru/watch/98737773'
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt='yandex'
+            />
+          </div>
+        </noscript>
       </body>
     </html>
   );
